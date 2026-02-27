bf81195d36313d0d386d37806b4b564f4bc6057a
Mahesh Munde <mundemahesh76@gmail.com>
2026-02-27 11:03:29 +0530

    fixed production port

--- app.py
+++ app.py
@@ -68,5 +68,7 @@ def contact():
 
 
 # ================= RUN APP =================
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
